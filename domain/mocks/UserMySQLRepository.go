0c3c471e9f5a74ca7d14097ce7686a40e2601269
Jerry <blawhi2435@gmail.com>
2022-01-15 14:58:57 +0800

    test: update user interface

--- domain/mocks/UserMySQLRepository.go
+++ domain/mocks/UserMySQLRepository.go
@@ -50,3 +50,45 @@ func (_m *UserMySQLRepository) Store(ctx context.Context, d *domain.User) error 
 
 	return r0
 }
+
+// UpdateBasicInfo provides a mock function with given fields: ctx, d
+func (_m *UserMySQLRepository) UpdateBasicInfo(ctx context.Context, d *domain.User) (int64, error) {
+	ret := _m.Called(ctx, d)
+
+	var r0 int64
+	if rf, ok := ret.Get(0).(func(context.Context, *domain.User) int64); ok {
+		r0 = rf(ctx, d)
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *domain.User) error); ok {
+		r1 = rf(ctx, d)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// UpdateImage provides a mock function with given fields: ctx, d
+func (_m *UserMySQLRepository) UpdateImage(ctx context.Context, d *domain.UserImage) (int64, error) {
+	ret := _m.Called(ctx, d)
+
+	var r0 int64
+	if rf, ok := ret.Get(0).(func(context.Context, *domain.UserImage) int64); ok {
+		r0 = rf(ctx, d)
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *domain.UserImage) error); ok {
+		r1 = rf(ctx, d)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
--- domain/mocks/UserUsecase.go
+++ domain/mocks/UserUsecase.go
@@ -5,6 +5,7 @@ package mocks
 import (
 	context "context"
 
+	domain "github.com/beecool-cocktail/application-backend/domain"
 	mock "github.com/stretchr/testify/mock"
 )
 
@@ -25,4 +26,69 @@ func (_m *UserUsecase) Logout(ctx context.Context, id int64) error {
 	}
 
 	return r0
+}
+
+// QueryById provides a mock function with given fields: ctx, id
+func (_m *UserUsecase) QueryById(ctx context.Context, id int64) (*domain.User, error) {
+	ret := _m.Called(ctx, id)
+
+	var r0 *domain.User
+	if rf, ok := ret.Get(0).(func(context.Context, int64) *domain.User); ok {
+		r0 = rf(ctx, id)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*domain.User)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, int64) error); ok {
+		r1 = rf(ctx, id)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// UpdateBasicInfo provides a mock function with given fields: ctx, d
+func (_m *UserUsecase) UpdateBasicInfo(ctx context.Context, d *domain.User) (int64, error) {
+	ret := _m.Called(ctx, d)
+
+	var r0 int64
+	if rf, ok := ret.Get(0).(func(context.Context, *domain.User) int64); ok {
+		r0 = rf(ctx, d)
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *domain.User) error); ok {
+		r1 = rf(ctx, d)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// UpdateImage provides a mock function with given fields: ctx, d
+func (_m *UserUsecase) UpdateImage(ctx context.Context, d *domain.UserImage) (int64, error) {
+	ret := _m.Called(ctx, d)
+
+	var r0 int64
+	if rf, ok := ret.Get(0).(func(context.Context, *domain.UserImage) int64); ok {
+		r0 = rf(ctx, d)
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *domain.UserImage) error); ok {
+		r1 = rf(ctx, d)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
 }
