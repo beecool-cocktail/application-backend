67bbc0147cecd627eaaa27c4e9e5740980d830f8
Jerry <blawhi2435@gmail.com>
2022-10-11 23:49:10 +0800

    fix: cocktail title too short

--- domain/cocktail.go
+++ domain/cocktail.go
@@ -89,7 +89,7 @@ type Cocktail struct {
 	ID          int64     `gorm:"type:bigint(64) NOT NULL auto_increment;primary_key"`
 	CocktailID  int64     `gorm:"type:bigint(64) NOT NULL;uniqueIndex:idx_cocktail_id"`
 	UserID      int64     `gorm:"type:bigint(64) NOT NULL;index:idx_user_id; comment: 作者id"`
-	Title       string    `gorm:"type:varchar(16) NOT NULL;; comment: 調酒名稱"`
+	Title       string    `gorm:"type:varchar(30) NOT NULL;; comment: 調酒名稱"`
 	Description string    `gorm:"type:varchar(512) NOT NULL; comment: 調酒介紹"`
 	Category    int       `gorm:"type:tinyint(1) NOT NULL DEFAULT 0; comment: 類型 0=草稿, 1=正式"`
 	CreatedDate time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;index:idx_date"`
